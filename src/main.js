fdc66de51a9d4e4ae118cd79ab30003e19dc1ff8
wuhu <1134436375@qq.com>
2020-03-24 16:52:42 +0800

    新建项目，第一次提交

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+// 导入vue构造函数
+import Vue from "vue";
+// 导入app vue组件 （.vue 结尾的就是组件）
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
