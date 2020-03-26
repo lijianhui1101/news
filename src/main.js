c515bb64eda3286897d72dd88c861e211cac1838
wuhu <1134436375@qq.com>
2020-03-26 22:54:06 +0800

    登录注册页完成有bug

--- src/main.js
+++ src/main.js
@@ -1,14 +1,31 @@
 // 导入vue构造函数
 import Vue from "vue";
-// 导入app vue组件 （.vue 结尾的就是组件）
+// 导入app vue组件 （.vue 结尾的都是组件）
 import App from "./App.vue";
+// 路由对象
 import router from "./router";
+// 导入vant ui组件库
+import Vant from "vant";
+// 导入axios
+import axios from "axios";
+
+// 绑定到原型，加上之后以后就可以在组件中通过this.$axios来调用请求方法
+Vue.prototype.$axios = axios;
+// 给axios添加基准路径，添加完了之后请求的url就会拼接这个地址
+axios.defaults.baseURL = "http://localhost:3000";
+
+// 注册vant插件 （vue.use都是用来注册插件）
+Vue.use(Vant);
 
 // 项目上线提示
-// 这里新增一行测试
+// 上线环境是否提示信息(忽略)
 Vue.config.productionTip = false;
 
+// 创建一个根实例
+// .$mount('#app') 相当于el配置，指定id为app的元素作为模板
 new Vue({
+  // 路由对象
   router,
+  // 加载第一个子组件，最底层的组件，（写法是固定的）
   render: h => h(App)
 }).$mount("#app");
